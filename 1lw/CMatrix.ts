d36cd8b5cf91f490fd1920d158110401f136b6c6
ZiF1R <do-alex03@mail.ru>
2022-02-08 21:32:39 +0300

    Add second constructor to CMatrix

--- 1lw/CMatrix.ts
+++ 1lw/CMatrix.ts
@@ -3,15 +3,33 @@ export class CMatrix {
   public columns: number;
   public matrix: number[][];
 
-  constructor(rows: number = 1, columns: number = 1) {
-    if (rows < 0 || columns < 0)
-      throw new RangeError("Size of matrix must be not negative number!");
+  // Default constructor,
+  // fill matrix with zeros
+  public constructor(rows: number, columns: number);
 
-    this.rows = rows;
-    this.columns = columns;
-    this.matrix = Array(this.rows).fill(0).map(
-        () => Array(this.columns).fill(0)
-    );
+  // Constructor for copying other matrix
+  public constructor(otherMatrix: CMatrix);
+
+  public constructor(...args: any[]) {
+    if (args.length === 2) {
+      let [ rows, columns ] = args;
+      if (rows < 0 || columns < 0)
+        throw new RangeError("Size of matrix must be not negative number!");
+
+      this.rows = rows;
+      this.columns = columns;
+      this.matrix = Array(this.rows).fill(0).map(
+          () => Array(this.columns).fill(0)
+      );
+    }
+    else {
+      let [ otherMatrix ] = args;
+      this.rows = otherMatrix.getRowsCount();
+      this.columns = otherMatrix.getColumnsCount();
+      this.matrix = Array(this.rows).fill(0).map(
+          () => Array(this.columns).fill(0)
+      );
+    }
   }
 
   getRowsCount(): number {
