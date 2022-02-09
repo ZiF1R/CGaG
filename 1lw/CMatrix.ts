540e03d4be673b709fb277d044a74e71f5403e56
ZiF1R <do-alex03@mail.ru>
2022-02-09 08:28:08 +0300

    Add getters and setters for CMatrix

--- 1lw/CMatrix.ts
+++ 1lw/CMatrix.ts
@@ -1,14 +1,10 @@
 export class CMatrix {
-  public rows: number;
-  public columns: number;
-  public matrix: number[][];
+  readonly rows: number;
+  readonly columns: number;
+  private matrix: number[][];
 
-  // Default constructor,
-  // fill matrix with zeros
-  public constructor(rows: number, columns: number);
-
-  // Constructor for copying other matrix
-  public constructor(otherMatrix: CMatrix);
+  public constructor(rows: number, columns: number); // Default constructor, fill matrix with zeros
+  public constructor(otherMatrix: CMatrix); // Constructor for copying other matrix
 
   public constructor(...args: any[]) {
     if (args.length === 2) {
@@ -26,10 +22,24 @@ export class CMatrix {
       let [ otherMatrix ] = args;
       this.rows = otherMatrix.getRowsCount();
       this.columns = otherMatrix.getColumnsCount();
-      this.matrix = Array(this.rows).fill(0).map(
-          () => Array(this.columns).fill(0)
-      );
+      this.matrix = otherMatrix.matrix;
     }
+  }
+
+  get Matrix(): number[][] {
+    return this.matrix;
+  }
+
+  set Matrix(matrix: number[][]) {
+    // check if the columns of assigned matrix have one size
+    let isEqualColumnsSize =
+      matrix.filter(arr => arr.length !== this.columns).length === 0;
+    
+    // if size of columns or rows doesn't match, throw error
+    if (matrix.length !== this.rows || !isEqualColumnsSize) {
+      throw new TypeError("Assigned matrix size doesn't match to aggregated matrix!");
+    }
+    else this.matrix = matrix;
   }
 
   getRowsCount(): number {
@@ -40,20 +50,20 @@ export class CMatrix {
     return this.columns;
   }
 
-  getRow(rowNumber: number): number[] | null {
-    if (rowNumber > this.rows || rowNumber < 0)
+  getRow(index: number): number[] | null {
+    if (index > this.rows || index < 0)
       return null;
-    return this.matrix[rowNumber];
+    return this.matrix[index];
   }
 
-  getColumn(columnNumber: number): number[] | null {
-    if (columnNumber > this.columns || columnNumber < 0)
+  getColumn(index: number): number[] | null {
+    if (index > this.columns || index < 0)
       return null;
     let resultColumn: Array<number> = [];
 
-    for (let row = 0; row < this.matrix.length; row++)
-      for (let column = 0; column < this.matrix[row].length; column++)
-        if (column === columnNumber)
+    for (let row = 0; row < this.rows; row++)
+      for (let column = 0; column < this.columns; column++)
+        if (column === index)
           resultColumn.push(this.matrix[row][column]);
 
     return resultColumn;
