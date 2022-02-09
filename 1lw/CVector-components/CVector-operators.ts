5eff44e9f94de63eeceb6fe479077ab38778b2af
ZiF1R <do-alex03@mail.ru>
2022-02-09 19:13:47 +0300

    Add realization of operators for CVector

---
+++ 1lw/CVector-components/CVector-operators.ts
@@ -0,0 +1,17 @@
+import { CVector } from "../CVector";
+
+CVector.prototype.Add = function(parameter: number | CVector): number[][] {
+  if (typeof parameter === 'object' && this.Rows !== parameter.Rows)
+    throw new Error("The size of two vectors must be equal!");
+
+  let result = JSON.parse(JSON.stringify(this.Matrix));
+  for (let row = 0; row < this.Rows; row++)
+    for (let column = 0; column < this.Columns; column++) {
+      if (typeof parameter === 'object')
+        result[row][column] = this.Matrix[row][column] + parameter.Matrix[row][column];
+      else
+        result[row][column] += parameter;
+    }
+
+  return result;
+}
