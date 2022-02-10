037173f0aa4982be150a32fe139f0d84726f2c98
ZiF1R <do-alex03@mail.ru>
2022-02-10 12:38:03 +0300

    Add tests for additional operations

--- 1lw/tests/index.test.ts
+++ 1lw/tests/index.test.ts
@@ -1,4 +1,11 @@
 import { A, B, V1, V2 } from "../index";
+import {
+  VectorMult,
+  ScalarMult,
+  ModOfVector,
+  CosBetweenVectors,
+  SphereToDecart
+} from "../AdditionalOperations";
 
 describe("Operations with CMatrix", (): void => {
   test("Addition of CMatrix", (): void => {
@@ -33,7 +40,29 @@ describe("Operations with CVector", (): void => {
   });
 });
 
-
-// describe("Test additional operations", (): void => {
-
-// });
+describe("Test additional operations", (): void => {
+  test("VectorMult", () => {
+    expect(VectorMult(V1, V2).Matrix).toStrictEqual([
+      [12],
+      [12],
+      [-8]
+    ]);
+  });
+  test("ScalarMult", () => {
+    expect(ScalarMult(V1, V2)).toBe(42);
+  });
+  test("ModOfVector", () => {
+    expect(ModOfVector(V1)).toBeCloseTo(6.7823299, 1);
+    expect(ModOfVector(V2)).toBeCloseTo(6.7823299, 1);
+  });
+  test("CosBetweenVectors", () => {
+    expect(CosBetweenVectors(V1, V2)).toBeCloseTo(0.877992, 1);
+  });
+  test("SphereToDecart", () => {
+    expect(SphereToDecart(V1).Matrix).toStrictEqual([
+      [0.28],
+      [-0.04],
+      [0.96]
+    ]);
+  });
+});
