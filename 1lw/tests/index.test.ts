10235001b6031bf526959d3037c4e2480a4d978e
ZiF1R <do-alex03@mail.ru>
2022-02-10 09:56:41 +0300

    Add tests

---
+++ 1lw/tests/index.test.ts
@@ -0,0 +1,39 @@
+import { A, B, V1, V2 } from "../index";
+
+describe("Operations with CMatrix", (): void => {
+  test("Addition of CMatrix", (): void => {
+    expect(A.Add(B).Matrix).toStrictEqual([
+      [ 5, 7, 9 ],
+      [ 5, 7, 9 ],
+      [ 14, 16, 18 ]
+    ]);
+  });
+  test("Multiplication of CMatrix", (): void => {
+    expect(A.Multiply(B).Matrix).toStrictEqual([
+      [ 27, 33, 39 ],
+      [ 63, 78, 93 ],
+      [ 99, 123, 147 ]
+    ]);
+  });
+  test("Addition of CMatrix and CVector", (): void => {
+    expect(A.Add(V1).Matrix).toStrictEqual([
+      [ 25 ],
+      [ 55 ],
+      [ 85 ]
+    ]);
+  });
+});
+
+describe("Operations with CVector", (): void => {
+  test("Multiplication of CVector", (): void => {
+    expect(V1.transpose().Multiply(V2).Matrix).toStrictEqual([[42]]);
+  });
+  test("Multiplication of CVector and CMatrix", (): void => {
+    expect(V1.transpose().Multiply(A).Multiply(V2).Matrix).toStrictEqual([[680]]);
+  });
+});
+
+
+// describe("Test additional operations", (): void => {
+
+// });
