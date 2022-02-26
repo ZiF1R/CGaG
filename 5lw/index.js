d3a7e5bd95c5b89a7b5f697799f0e1e366b527f9
ZiF1R <do-alex03@mail.ru>
2022-02-26 09:52:33 +0300

    Add canvas to page and function drawing the pyramid

--- 5lw/index.js
+++ 5lw/index.js
@@ -2,19 +2,30 @@ import { CMatrix } from "./node_modules/cmatrix-cvector/CMatrix.js";
 import { CVector } from "./node_modules/cmatrix-cvector/CVector.js";
 import { ScalarMult, VectorMult } from "./node_modules/cmatrix-cvector/AdditionalOperations.js";
 
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+class CPoint {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+}
+
 class CPyramid {
   constructor() {
-    this.Vertexes = new CMatrix(3, 6);
+    this.Vertexes = new CMatrix(4, 6);
 
     // Default coordinates for pyramid vertexes.
-    // In first row contains x coordinates,
+    // First row contains x coordinates,
     // in second - y coordinates,
     // in third - z coordinates,
     // e.g. point A -> first column => (6, 0, 0)
     this.Vertexes.Matrix = [
       [6, 0, -6, 2, 0, -2],
       [0, -6, 0, 0, -2, 0],
-      [0, 0, 0, 6, 6, 6]
+      [0, 0, 0, 6, 6, 6],
+      [0, 0, 0, 0, 0, 0]
     ];
   }
 
@@ -22,18 +33,70 @@ class CPyramid {
 
   }
 
-  DrawWithoutInvisibleLines(pointOfView, viewArea) {
+  /**
+   * @param {CMatrix} viewPoint contains (x, y, z) coordinates in decart coordinate system
+   * @param {Array<Array<number>>} viewArea two dimentional array, contains coordinates of top left and bottom right corners
+   */
+  DrawWithoutInvisibleLines(viewPoint, viewArea) {
+    let VM = CreateViewCoord(
+      viewPoint.Matrix[0][0],
+      viewPoint.Matrix[1][0],
+      viewPoint.Matrix[2][0],
+    );
 
+    let viewVertexes = VM.Multiply(this.Vertexes);
+
+    let y_max = Math.max(...viewVertexes.Matrix[1]),
+      y_min = Math.min(...viewVertexes.Matrix[1]),
+      x_max = Math.max(...viewVertexes.Matrix[0]),
+      x_min = Math.min(...viewVertexes.Matrix[0]);
+
+    let MW = SpaceToWindow(
+      [[x_min, y_min], [x_max, y_max]],
+      [[50, 50], [450, 450]]
+    );
+
+    // go through the vertexes
+    let temp = new CVector(3);
+    let points = [];
+    for (let i = 0; i < this.Vertexes.Matrix[0].length; i++) {
+      temp.Matrix = [
+        [viewVertexes.Matrix[0][i]],
+        [viewVertexes.Matrix[1][i]],
+        [1]
+      ];
+      temp = MW.Multiply(temp);
+      points.push(new CPoint(temp.Matrix[0][0], temp.Matrix[1][0]));
+    }
+
+    ctx.moveTo(points[2].x, points[2].y);
+    for (let i = 0; i < 3; i++)
+      ctx.lineTo(points[i].x, points[i].y);
+
+    ctx.moveTo(points[5].x, points[5].y);
+    for (let i = 3; i < 6; i++)
+      ctx.lineTo(points[i].x, points[i].y);
+    
+    for (let i = 0; i < 3; i++){
+      ctx.moveTo(points[i].x, points[i].y);
+      ctx.lineTo(points[i + 3].x, points[i + 3].y);
+    }
+    
+    ctx.lineWidth = "1";
+    ctx.strokeStyle = "#000";
+    ctx.lineCap = 'round';
+    ctx.lineJoin = 'round';
+    ctx.stroke();
   }
 
-  DrawWithInvisibleLines(pointOfView, viewArea) {
+  DrawWithInvisibleLines(viewPoint, viewArea) {
 
   }
 }
 
 /**
- * @param {Array<number>} areaInWorldCoordinates 
- * @param {Array<number>} areaInWindowCoordinates 
+ * @param {Array<Array<number>>} areaInWorldCoordinates two dimentional array, contains coordinates of top left and bottom right corners
+ * @param {Array<Array<number>>} areaInWindowCoordinates two dimentional array, contains coordinates of top left and bottom right corners
  */
 function SpaceToWindow(areaInWorldCoordinates, areaInWindowCoordinates) {
   let [x_min, y_min] = areaInWorldCoordinates[0];
@@ -56,6 +119,15 @@ function SpaceToWindow(areaInWorldCoordinates, areaInWindowCoordinates) {
 
   return Tsw;
 }
+
+const pyramid = new CPyramid();
+let viewPoint = new CVector(3);
+viewPoint.Matrix = [
+  [0],
+  [0],
+  [0]
+]
+pyramid.DrawWithoutInvisibleLines(viewPoint);
 
 /**
  * @param {number} r
